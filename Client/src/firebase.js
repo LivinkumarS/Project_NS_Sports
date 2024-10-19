45bb47ff951ef14479373a3d3686fe2f9da395c9
LivinkumarS <001liv100@ggmail.com>
2024-10-19 14:34:18 +0530

    firebase api key secure correction

--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAFkvM9pqLscthl7i-FUtyoAnAb7ssyC4g",
+  apiKey: import.meta.env.VITE_OAUTH_KEY,
   authDomain: "ns-sports.firebaseapp.com",
   projectId: "ns-sports",
   storageBucket: "ns-sports.appspot.com",
   messagingSenderId: "457911931192",
   appId: "1:457911931192:web:c5a034e85c43af784d31cd",
-  measurementId: "G-DC0PD3G5EZ"
+  measurementId: "G-DC0PD3G5EZ",
 };
 
 // Initialize Firebase
